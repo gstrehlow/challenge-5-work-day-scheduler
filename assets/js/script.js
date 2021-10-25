5ed7086fe1d824f2c910160ceef64435cc317e27
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2021-10-24 21:30:23 -0500

    added click listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,14 @@
 var dateToday = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(dateToday);
 
+$(document).ready(function () {
+    // this function will allow the saveBtn to be clickable with a click listener, retreive values of the description, and save to local storage
+    $(".saveBtn").on("click", function() {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+
+
+})
