1e757c3f3f75545380babe54d62c4c1b4b4c48ed
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2021-10-24 22:02:09 -0500

    added css classes dynamically according to criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,5 +11,31 @@ $(document).ready(function () {
         localStorage.setItem(time, text);
     })
 
+    function timeTrack() {
+        var currentTime = moment().hour();
 
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+            // if and else if statements to check the time and add the classes accordingly to meet the acceptance criteria
+            if (timeBlock < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (timeBlock === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    timeTrack();
 })
